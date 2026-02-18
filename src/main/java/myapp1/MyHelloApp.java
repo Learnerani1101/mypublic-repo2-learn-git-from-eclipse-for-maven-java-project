a7ba088728a81dd5427a8c3f2313cdf888614d03
Anindita Majumdar <anigitlearning@gmail.com>
2026-02-18 18:24:43 +0530

    First Commit for Sample Maven Project

---
+++ src/main/java/myapp1/MyHelloApp.java
@@ -0,0 +1,10 @@
+package myapp1;
+
+public class MyHelloApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World From my sample Maven Project");
+	}
+
+}
